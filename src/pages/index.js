f38ad5fb2b09c0b05e0b44911a049a7a2316326b
Winston Wu <winxton@gmail.com>
2024-02-02 19:51:00 -0500

    Update tagline, add images.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,22 +7,28 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
+const WeekView = require('@site/static/img/week.svg').default
+
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
+
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="http://eepurl.com/g__xeb">
+            Sign up for beta
           </Link>
         </div>
+
+        <WeekView/>
       </div>
     </header>
   );
